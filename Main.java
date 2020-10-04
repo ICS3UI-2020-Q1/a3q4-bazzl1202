1507ef62e21b3a6a00a12bc92d4a8dea71869d78
Adam Lamont <adam_lamont@wrdsb.ca>
2020-10-04 13:37:27 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * Asks for postive integer and tells you it in written form
+ * @author Liana Bazzarella
  */
 public class Main {
 
@@ -9,7 +10,54 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // create a scanner for user input
+    Scanner input = new Scanner(System.in);
+
+    // ask the user for a positive integer
+    System.out.println("Please enter a positive integer:");
+    int positiveInteger = input.nextInt();
+
+    switch(positiveInteger){
+      case 1:
+      System.out.println("one");
+      break;
+      case 2:
+      System.out.println("two");
+      break;
+      case 3:
+      System.out.println("three");
+      break;
+      case 4:
+      System.out.println("four");
+      break;
+      case 5:
+      System.out.println("five");
+      break;
+      case 6:
+      System.out.println("six");
+      break;
+      case 7:
+      System.out.println("seven");
+      break;
+      case 8:
+      System.out.println("eight");
+      break;
+      case 9:
+      System.out.println("nine");
+      break;
+      case 10:
+      System.out.println("ten");
+      break;
+      default:
+      System.out.println("not in the range");
+    }
+
+    
+
+
+   
+
+    }
     
   }
-}
+
